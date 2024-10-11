a97991918ef4d67cde43780c3c1ff020bcf4cde2
massivebird <33991373+massivebird@users.noreply.github.com>
2024-10-11 18:33:02 -0400

    feat: try_from and try_into between inst and i8

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,18 @@ impl Computer {
         Self::default()
     }
 
-    fn execute_instructions(&mut self, insts: [Inst; 16]) {
-        for inst in insts {
+    fn load_instructions(&mut self, insts: [Inst; 16]) {
+        for (idx, inst) in insts.into_iter().enumerate() {
+            self.memory[idx] = inst.try_into().unwrap();
+        }
+    }
+
+    fn run(&mut self) {
+        for val in self.memory {
+            let Ok(inst) = Inst::try_from(val) else {
+                continue;
+            };
+            dbg!(&inst);
             // 000    add n        [D0] <- [D0] + [n]
             // 001    and n        [D0] <- [D0] & [n]
             // 010    shl n        [n] <- [n] << 1
@@ -29,7 +39,7 @@ impl Computer {
                 Inst::Load(n) => self.register = self.memory[n as usize],
                 Inst::Str(n) => self.memory[n as usize] = self.register,
                 Inst::Jmp(n) => self.pc = n,
-                Inst::Jz(n) => todo!(), // is z the control code?
+                Inst::Jz(_n) => todo!(), // is z the control code?
                 Inst::None => (),
             }
 
@@ -39,6 +49,7 @@ impl Computer {
 }
 
 #[allow(dead_code)]
+#[derive(Debug, Copy, Clone)]
 enum Inst {
     Add(i8),
     And(i8),
@@ -49,6 +60,44 @@ enum Inst {
     Jmp(u8),
     Jz(u8),
     None,
+}
+
+impl TryFrom<i8> for Inst {
+    type Error = ();
+
+    fn try_from(value: i8) -> Result<Self, Self::Error> {
+        if value < 16 {
+            return Ok(Self::Add(value));
+        }
+
+        match format!("{value:x}").chars().nth(0).unwrap() {
+            '1' => Ok(Self::And(value)),
+            '2' => Ok(Self::Shl(value as u8)),
+            '3' => Ok(Self::Disp(value)),
+            '4' => Ok(Self::Str(value)),
+            '5' => Ok(Self::Jmp(value as u8)),
+            '6' => Ok(Self::Jz(value as u8)),
+            _ => Err(()),
+        }
+    }
+}
+
+impl TryInto<i8> for Inst {
+    type Error = ();
+
+    fn try_into(self) -> Result<i8, Self::Error> {
+        match self {
+            Self::Add(n) => Ok(n),
+            Self::And(n) => Ok(n + 16),
+            Self::Shl(n) => Ok(i8::try_from(n + 16 * 2).unwrap()),
+            Self::Disp(n) => Ok(n + 16 * 3),
+            Self::Load(n) => Ok(n + 16 * 4),
+            Self::Str(n) => Ok(n + 16 * 5),
+            Self::Jmp(n) => Ok(i8::try_from(n + 16 * 6).unwrap()),
+            Self::Jz(n) => Ok(i8::try_from(n + 16 * 7).unwrap()),
+            Self::None => Ok(0),
+        }
+    }
 }
 
 fn main() {
@@ -72,7 +121,8 @@ fn main() {
     ];
 
     let mut c = Computer::new();
-    c.execute_instructions(insts);
+    c.load_instructions(insts);
+    c.run();
 
     dbg!(c);
 }
